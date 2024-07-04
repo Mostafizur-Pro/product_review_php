980fbce4a19d2140890090613f004547ebb45ca5
mostafizur-pro <mostafizur0195@gmail.com>
2024-07-04 11:01:20 +0600

    navbar add

--- index.php
+++ index.php
@@ -1,6 +1,31 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
 
-include "footer.php";
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product Review</title>
 
+    <!-- Navbar -->
+    <link rel="stylesheet" type="text/css" media="screen" href="css/navbar.css" />
+</head>
 
-?>
+<body>
+    <!-- Navbar Section -->
+    <div>
+        <?php
+        include_once "navbar.php";
+        ?>
+    </div>
+    <div>
+        Index
+    </div>
+
+    <div>
+        <?php
+        include_once "footer.php";
+        ?>
+    </div>
+</body>
+
+</html>
---
+++ navbar.php
@@ -0,0 +1,20 @@
+<nav>
+    <div class="logo">
+        <h2>Logo</h2>
+    </div>
+    <div class="nav-items">
+        <ul class="overview">
+            <li> <a href="#"> Home </a> </li>
+            <li><a href="#"> Product</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Dashboard</a></li>
+        </ul>
+        <ul class="account">
+            <li><a href="#">Login </a></li>
+        </ul>
+    </div>
+
+
+</nav>
+<div class="hero-section"></div>
